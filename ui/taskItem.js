4d0427b96048c30983f32a34d4dd5621e9ebbc61
sleepsemek <bayukovvdaniil@gmail.com>
2025-11-09 13:59:30 +0300

    Added styles and accessibility

--- ui/taskItem.js
+++ ui/taskItem.js
@@ -8,26 +8,25 @@ export function createTaskItem(
     }
 ) {
     const li = Object.assign(document.createElement('li'), {
-        className: 'tasks__item task'
+        className: `tasks__item task ${isCompleted ? 'task--completed' : ''}`
     })
 
     li.dataset.taskId = id
     li.innerHTML = `
-        <header class="task__header">
-            <h3 class="task__title">${name}</h3>
-            <p class="task__description">${info}</p>
-        </header>
+        <h3 class="task__title">${name}</h3>
+        <p class="task__description">${info}</p>
+        
         <div class="task__controls">
-            <label class="task__controls-label label">
-                <input class="task__controls-input input" type="checkbox" ${isImportant ? 'checked' : ''} data-important-checkbox>
+            <label class="task__controls-label label label--inline">
+                <input class="task__controls-input input" type="checkbox" aria-label="Пометить задачу '${name}' как важную" ${isImportant ? 'checked' : ''} data-important-checkbox>
                 Важная
             </label>
-            <label class="task__controls-label label">
-                <input class="task__controls-input input" type="checkbox" ${isCompleted ? 'checked' : ''} data-complete-checkbox>
+            <label class="task__controls-label label label--inline">
+                <input class="task__controls-input input" type="checkbox" aria-label="Пометить задачу '${name}' как завершенную" ${isCompleted ? 'checked' : ''} data-complete-checkbox>
                 Выполнена
             </label>
         </div>
-        <button class="task__controls-button button button--delete" type="button" data-delete-button>Удалить</button>
+        <button class="task__controls-button button" type="button" aria-label="Удалить задачу '${name}'" data-delete-button>Удалить</button>
     `
     return li
 }
